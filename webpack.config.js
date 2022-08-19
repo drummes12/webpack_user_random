78195fbfab91ebe56037a95b80ce4e0d7e5c6017
drummes12 <daesgodu@gmail.com>
2022-08-19 11:13:54 -0500

    Webpack modo produccion

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -58,6 +59,7 @@ module.exports = {
       filename: "assets/[name].[contenthash].css",
     }),
     new Dotenv(),
+    new CleanWebpackPlugin(),
   ],
   optimization: {
     minimizer: true,
